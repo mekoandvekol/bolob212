7c6d9696d0e2d24d5206a806a3cf6d7eeb0b9662
mekoandvekol <43092328+mekoandvekol@users.noreply.github.com>
2018-09-08 18:40:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -276,7 +276,13 @@ if (message.content.startsWith("L-cv")) {
 
 
 
-
+client.on('message', message => {
+    if (message.content.startsWith("L-bans")) {
+        message.guild.fetchBans()
+        .then(bans => message.channel.send(`${bans.size} عدد اشخاص المبندة من السيرفر `))
+  .catch(console.error);
+}
+});
 
 
 client.login(process.env.BOT_TOKEN);
