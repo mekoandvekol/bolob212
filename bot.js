cd402f50f928922f99d33fef978da0e76b069502
mekoandvekol <43092328+mekoandvekol@users.noreply.github.com>
2018-09-08 18:38:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -252,7 +252,15 @@ var prefix = "L-";
 });
 
 
+client.on("message", (message) => {
+if (message.content.startsWith("L-ct")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'text');
+message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
 
+}
+});
 
 
 
