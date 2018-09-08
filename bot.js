952c1f2d679523b98ccd955849f93b3210665a76
mekoandvekol <43092328+mekoandvekol@users.noreply.github.com>
2018-09-08 18:40:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -264,7 +264,15 @@ message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
 
 
 
-
+client.on("message", (message) => {
+if (message.content.startsWith("L-cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
 
 
 
